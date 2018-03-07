d7e679e314bdc51bb009f4947f729a1647e0a2fd
Alexander Liljengård <alexander@paxxmedia.se>
2018-03-07 22:07:09 +0100

    added tests

---
+++ test/deepKeys.test.js
@@ -0,0 +1,38 @@
+import deepKeys from '../src/helpers/deepKeys';
+
+describe('deepKeys', () => {
+  it('can resolve both objects and arrays', () => {
+    const resolve = {
+      find: {
+        thisDeep: {
+          key: {
+            but: [
+              { also: 'these' },
+              { and: 'this one' },
+            ],
+          },
+        },
+      },
+    };
+
+    expect(deepKeys(resolve)).toEqual([
+      'find.thisDeep.key.but.0.also',
+      'find.thisDeep.key.but.1.and',
+    ]);
+  });
+
+  it('correctly resolves arrays', () => {
+    const resolve = {
+      deep: {
+        deeper: {
+          deepest: ['value', 'another value', { a: 'third value' }],
+        },
+      },
+    };
+
+    expect(deepKeys(resolve)).toEqual([
+      'deep.deeper.deepest',
+      'deep.deeper.deepest.2.a',
+    ]);
+  });
+});
